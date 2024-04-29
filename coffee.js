f5f5f292c371dfa30f62c845889dc85212d6884c
Pochtareva14 <168444435+Pochtareva14@users.noreply.github.com>
2024-04-29 16:37:06 +0300

    Create coffee.js

---
+++ coffee.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    const modal = $('#settingsModal');
+
+    const btn = $('#settingsBtn');
+
+    const span = $('.close');
+
+    btn.click(function() {
+
+        modal.css('display', 'block');
+
+    });
+
+    span.click(function() {
+
+        modal.css('display', 'none');
+
+    })
+
+//хз че с корзиной
+
+});
